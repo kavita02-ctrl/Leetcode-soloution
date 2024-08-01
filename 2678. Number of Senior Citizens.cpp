81f9b23f6f1124ad2beeec1af05b63895c60d887
Kavita Dhar <dharkavita123@gmail.com>
2024-08-02 02:51:31 +0530

    Update 2678. Number of Senior Citizens.cpp

--- 2678. Number of Senior Citizens.cpp
+++ 2678. Number of Senior Citizens.cpp
@@ -2,13 +2,13 @@
 class Solution {
 public:
     int countSeniors(vector<string>& details) {
-        int a = 0;
+        int count=0;
         for(const string & detail : details) {
             int age =stoi(detail.substr(11, 2));
 
-            if(age>=60)
-            a++;
+            if(age>60)
+            count++;
         }
-        return a;
+        return count;
     }
 };
